a22ace9bf362ed6b640c26e5b6542b97de1c627f
tavinho0207 <0000109258819xsp@al.educacao.sp.gov.br>
2024-11-07 09:52:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital dos Estados Unidos?',
+    'Washington, D.C.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é fotossíntese?',
+    'Processo pelo qual as plantas convertem luz solar em energia.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem inventou o avião?',
+    'Alberto Santos Dumont'
+)
+
+criaCartao(
+    'História',
+    'Em que ano começou a Primeira Guerra Mundial?',
+    '1914'
+)
+
+criaCartao(
+    'Cultura Geral',
+    'Qual é o esporte mais praticado no mundo?',
+    'Futebol'
+)
